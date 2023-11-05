e544179d51e4a4631f9b6ec3ac1bbb68f3f99317
MelifaroMax <144232233+MelifaroMax@users.noreply.github.com>
2023-11-05 15:36:00 +0300

    Add files via upload

---
+++ functionlanguagerus.js
@@ -0,0 +1,35 @@
+const node_for_click = document.getElementById("eng click")
+
+if (document.getElementById("gen").innerText = 'man')
+{function find_edit(){
+    
+    document.getElementById("eng click").innerText = 'eng'
+    var t = document.getElementById("eng click").innerText;
+    console.log(t)
+
+    document.getElementById("gen").innerText = 'man'
+    var gen = document.getElementById("gen").innerText;
+    console.log(gen)
+
+    document.getElementById("thirdname").innerText = 'Ivanovich'
+    var thirdname = document.getElementById("thirdname").innerText;
+    console.log(thirdname)
+
+    document.getElementById("secondname").innerText = 'Ivanovich'
+    var secondname = document.getElementById("secondname").innerText;
+    console.log(secondname)
+
+    document.getElementById("firstname").innerText = 'Ivanovich'
+    var firstname = document.getElementById("firstname").innerText;
+    console.log(firstname)
+
+    document.getElementById("place").innerText = 'THE FEDERAL MIGRATION SERVICE OF RUSSIA IN THE CITY OF MOSCOW'
+    var place = document.getElementById("place").innerText;
+    console.log(place)
+
+    document.getElementById("city").innerText = 'Moscow'
+    var city = document.getElementById("city").innerText;
+    console.log(city)}
+}
+
+node_for_click.addEventListener("click",find_edit)
